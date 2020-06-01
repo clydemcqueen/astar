558dbd9f14b2a7b0a49d1ba62390dd0de1514563
Clyde McQueen <clyde@mcqueen.net>
2020-06-01 10:47:15 -0700

    Pass ament_lint_common linters

--- include/astar/astar.hpp
+++ include/astar/astar.hpp
@@ -1,118 +1,151 @@
-#ifndef ORCA_BASE_ASTAR_HPP
-#define ORCA_BASE_ASTAR_HPP
+// Copyright (c) 2020, Clyde McQueen.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#include <vector>
+#ifndef ASTAR__ASTAR_HPP_
+#define ASTAR__ASTAR_HPP_
+
 #include <cassert>
 #include <iostream>
-#include <queue>
-#include <map>
-#include <limits>
 #include <functional>
+#include <limits>
+#include <map>
+#include <queue>
+#include <utility>
+#include <vector>
 
 namespace astar
 {
 
-  // Nodes
-  using node_type = int;
+// Nodes
+using node_type = int;
 
-  // Edges
-  struct Edge
+// Edges
+struct Edge
+{
+  node_type a;
+  node_type b;
+
+  // Distance between a and b, in either direction
+  double distance;
+
+  Edge(node_type _a, node_type _b, double _distance)
+  : a{_a}, b{_b}, distance{_distance} {}
+};
+
+// Used for get_neighbors
+struct Neighbor
+{
+  node_type node;
+
+  // Distance to this neighbor
+  double distance;
+
+  Neighbor(node_type _node, double _distance)
+  : node{_node}, distance{_distance} {}
+};
+
+// Graph
+struct Graph
+{
+  std::vector<Edge> edges_;
+
+  explicit Graph(std::vector<Edge> graph)
+  : edges_{std::move(graph)} {}
+
+  std::vector<Neighbor> get_neighbors(node_type node);
+};
+
+// Nodes in the open_set_ contain additional state
+struct CandidateNode
+{
+  node_type node;
+
+  // Cost of cheapest path from start to this node
+  double g_score;
+
+  // Heuristic for cost of cheapest path from start through this node to the destination
+  // f_score = g_score + h_(this)
+  double f_score;
+
+  // Must be default constructible to be used in a priority queue
+  CandidateNode() = default;
+
+  // Useful constructor
+  CandidateNode(node_type _node, double _g_score, double h)
+  : node{_node}, g_score{_g_score}, f_score{_g_score + h} {}
+
+  // Priority queue uses value operator as comparator
+  bool operator()(const CandidateNode & a, const CandidateNode & b)
   {
-    node_type a;
-    node_type b;
+    return a.f_score > b.f_score;
+  }
+};
 
-    // Distance between a and b, in either direction
-    double distance;
+std::ostream & operator<<(std::ostream & os, CandidateNode const & c);
 
-    Edge(node_type _a, node_type _b, double _distance) : a{_a}, b{_b}, distance{_distance}
-    {}
-  };
+// Find the shortest path in a graph
+class Solver
+{
+  Graph graph_;
 
-  // Used for get_neighbors
-  struct Neighbor
-  {
-    node_type node;
+  // Heuristic function provides a rough guess of distance between 2 nodes,
+  // must be < than actual distance
+  using HeuristicFn = std::function<double (node_type a, node_type b)>;
 
-    // Distance to this neighbor
-    double distance;
+  HeuristicFn h_;
 
-    Neighbor(node_type _node, double _distance) : node{_node}, distance{_distance}
-    {}
-  };
+  // Best g_score from start to a node
+  std::map<node_type, double> best_g_score_;
 
-  // Graph
-  struct Graph
-  {
-    std::vector<Edge> edges_;
+  // Parent nodes, used to reconstruct the path at the end
+  std::map<node_type, node_type> best_parents_;
 
-    explicit Graph(std::vector<Edge> graph) : edges_{std::move(graph)}
-    {}
+  // Set of candidate nodes
+  std::priority_queue<CandidateNode, std::vector<CandidateNode>, CandidateNode> open_set_;
 
-    std::vector<Neighbor> get_neighbors(node_type node);
-  };
+  void print_open_set();
 
-  // Nodes in the open_set_ contain additional state
-  struct CandidateNode
-  {
-    node_type node;
+  void reconstruct_path(node_type node, std::vector<node_type> & path);
 
-    // Cost of cheapest path from start to this node
-    double g_score;
+  void reset();
 
-    // Heuristic for cost of cheapest path from start through this node to the destination
-    // f_score = g_score + h_(this)
-    double f_score;
+public:
+  explicit Solver(std::vector<Edge> edges, HeuristicFn h)
+  : graph_{Graph(std::move(edges))}, h_{std::move(h)} {}
 
-    // Must be default constructible to be used in a priority queue
-    CandidateNode() = default;
+  // Find the best path from start to destination, return true if successful
+  bool find_shortest_path(node_type start, node_type destination, std::vector<node_type> & result);
+};
 
-    // Useful constructor
-    CandidateNode(node_type _node, double _g_score, double h) : node{_node}, g_score{_g_score}, f_score{_g_score + h}
-    {}
+}  // namespace astar
 
-    // Priority queue uses value operator as comparator
-    bool operator()(const CandidateNode &a, const CandidateNode &b)
-    {
-      return a.f_score > b.f_score;
-    }
-  };
-
-  std::ostream &operator<<(std::ostream &os, CandidateNode const &c);
-
-  // Find the shortest path in a graph
-  class Solver
-  {
-    Graph graph_;
-
-    // Heuristic function provides a rough guess of distance between 2 nodes, must be < than actual distance
-    using HeuristicFn = std::function<double(node_type a, node_type b)>;
-
-    HeuristicFn h_;
-
-    // Best g_score from start to a node
-    std::map<node_type, double> best_g_score_;
-
-    // Parent nodes, used to reconstruct the path at the end
-    std::map<node_type, node_type> best_parents_;
-
-    // Set of candidate nodes
-    std::priority_queue<CandidateNode, std::vector<CandidateNode>, CandidateNode> open_set_;
-
-    void print_open_set();
-
-    void reconstruct_path(node_type node, std::vector<node_type> &path);
-
-    void reset();
-
-  public:
-
-    explicit Solver(std::vector<Edge> edges, HeuristicFn h) : graph_{Graph(std::move(edges))}, h_{std::move(h)}
-    {}
-
-    // Find the best path from start to destination, return true if successful
-    bool find_shortest_path(node_type start, node_type destination, std::vector<node_type> &result);
-  };
-
-} // namespace astar
-
-#endif //ORCA_BASE_ASTAR_HPP
+#endif  // ASTAR__ASTAR_HPP_
--- src/astar.cpp
+++ src/astar.cpp
@@ -1,125 +1,161 @@
+// Copyright (c) 2020, Clyde McQueen.
+// All rights reserved.
+//
+// Software License Agreement (BSD License 2.0)
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 #include "astar/astar.hpp"
 
 #include <iomanip>
+#include <limits>
+#include <queue>
+#include <vector>
 
 namespace astar
 {
 
-  std::ostream &operator<<(std::ostream &os, CandidateNode const &c)
-  {
-    return os << std::fixed << std::setprecision(2)
-              << "{marker: " << c.node << ", g_score: " << c.g_score << ", f_score: " << c.f_score << "}";
+std::ostream & operator<<(std::ostream & os, CandidateNode const & c)
+{
+  return os << std::fixed << std::setprecision(2) <<
+         "{marker: " << c.node << ", g_score: " << c.g_score << ", f_score: " << c.f_score << "}";
+}
+
+// Return the neighbors of this node
+std::vector<Neighbor> Graph::get_neighbors(node_type node)
+{
+  std::vector<Neighbor> neighbors;
+
+  for (auto edge : edges_) {
+    if (edge.a == node) {
+      neighbors.emplace_back(edge.b, edge.distance);
+    } else if (edge.b == node) {
+      neighbors.emplace_back(edge.a, edge.distance);
+    }
   }
 
-  // Return the neighbors of this node
-  std::vector<Neighbor> Graph::get_neighbors(node_type node)
-  {
-    std::vector<Neighbor> neighbors;
+  return neighbors;
+}
 
-    for (auto edge : edges_) {
-      if (edge.a == node) {
-        neighbors.emplace_back(edge.b, edge.distance);
-      } else if (edge.b == node) {
-        neighbors.emplace_back(edge.a, edge.distance);
+void Solver::print_open_set()
+{
+  auto temp = open_set_;
+
+  std::cout << "queue: ";
+  while (!temp.empty()) {
+    auto c = temp.top();
+    temp.pop();
+    std::cout << c << ", ";
+  }
+  std::cout << std::endl;
+}
+
+// Follow the parent links to reconstruct the shortest path
+void Solver::reconstruct_path(node_type node, std::vector<node_type> & path)
+{
+  if (best_parents_.find(node) != best_parents_.end()) {
+    reconstruct_path(best_parents_[node], path);
+  }
+  std::cout << node << ", ";
+  path.push_back(node);
+}
+
+void Solver::reset()
+{
+  // Clear everything
+  best_g_score_.clear();
+  best_parents_.clear();
+  open_set_ = std::priority_queue<CandidateNode, std::vector<CandidateNode>, CandidateNode>();
+
+  // Initialize best_g_score_ with ~infinity
+  for (auto edge : graph_.edges_) {
+    best_g_score_[edge.a] = std::numeric_limits<double>::max();
+    best_g_score_[edge.b] = std::numeric_limits<double>::max();
+  }
+}
+
+// Find the best path from start to destination, return true if successful
+bool Solver::find_shortest_path(
+  node_type start, node_type destination,
+  std::vector<node_type> & result)
+{
+  reset();
+
+  // Best distance to start node is always 0
+  best_g_score_[start] = 0;
+
+  // Add start node to open set
+  open_set_.push(CandidateNode(start, 0, h_(start, destination)));
+
+  while (!open_set_.empty()) {
+    // print_open_set();
+
+    // Pop the path with the best f_score
+    auto current = open_set_.top();
+    open_set_.pop();
+    // std::cout << "pop " << current << std::endl;
+
+    // Are we done?
+    if (current.node == destination) {
+      result.clear();
+      std::cout << "reconstructed path: ";
+      reconstruct_path(destination, result);
+      std::cout << std::endl;
+      return true;
+    }
+
+    // If this path to current.node is worse than the best one we've seen, drop it
+    if (current.g_score > best_g_score_[current.node]) {
+      continue;
+    }
+
+    // Loop through neighbors
+    auto neighbors = graph_.get_neighbors(current.node);
+    // std::cout << "considering " << neighbors.size() << " neighbors" << std::endl;
+    for (auto neighbor : neighbors) {
+      // tentative_g_score is the distance from start through current to the neighbor
+      double tentative_g_score = best_g_score_[current.node] + neighbor.distance;
+
+      // Does this beat the current best path?
+      if (tentative_g_score < best_g_score_[neighbor.node]) {
+        // Yes! Remember this path
+        best_g_score_[neighbor.node] = tentative_g_score;
+        best_parents_[neighbor.node] = current.node;
+
+        // Add the neighbor to the open set
+        auto c = CandidateNode(neighbor.node, tentative_g_score, h_(neighbor.node, destination));
+        // std::cout << "push " << c << std::endl;
+        open_set_.push(c);
       }
     }
-
-    return neighbors;
   }
 
-  void Solver::print_open_set()
-  {
-    auto temp = open_set_;
+  return false;
+}
 
-    std::cout << "queue: ";
-    while (!temp.empty()) {
-      auto c = temp.top();
-      temp.pop();
-      std::cout << c << ", ";
-    }
-    std::cout << std::endl;
-  }
-
-  // Follow the parent links to reconstruct the shortest path
-  void Solver::reconstruct_path(node_type node, std::vector<node_type> &path)
-  {
-    if (best_parents_.find(node) != best_parents_.end()) {
-      reconstruct_path(best_parents_[node], path);
-    }
-    std::cout << node << ", ";
-    path.push_back(node);
-  }
-
-  void Solver::reset()
-  {
-    // Clear everything
-    best_g_score_.clear();
-    best_parents_.clear();
-    open_set_ = std::priority_queue<CandidateNode, std::vector<CandidateNode>, CandidateNode>();
-
-    // Initialize best_g_score_ with ~infinity
-    for (auto edge : graph_.edges_) {
-      best_g_score_[edge.a] = std::numeric_limits<double>::max();
-      best_g_score_[edge.b] = std::numeric_limits<double>::max();
-    }
-  }
-
-  // Find the best path from start to destination, return true if successful
-  bool Solver::find_shortest_path(node_type start, node_type destination, std::vector<node_type> &result)
-  {
-    reset();
-
-    // Best distance to start node is always 0
-    best_g_score_[start] = 0;
-
-    // Add start node to open set
-    open_set_.push(CandidateNode(start, 0, h_(start, destination)));
-
-    while (!open_set_.empty()) {
-      // print_open_set();
-
-      // Pop the path with the best f_score
-      auto current = open_set_.top();
-      open_set_.pop();
-      // std::cout << "pop " << current << std::endl;
-
-      // Are we done?
-      if (current.node == destination) {
-        result.clear();
-        std::cout << "reconstructed path: ";
-        reconstruct_path(destination, result);
-        std::cout << std::endl;
-        return true;
-      }
-
-      // If this path to current.node is worse than the best one we've seen, drop it
-      if (current.g_score > best_g_score_[current.node]) {
-        continue;
-      }
-
-      // Loop through neighbors
-      auto neighbors = graph_.get_neighbors(current.node);
-      // std::cout << "considering " << neighbors.size() << " neighbors" << std::endl;
-      for (auto neighbor : neighbors) {
-        // tentative_g_score is the distance from start through current to the neighbor
-        double tentative_g_score = best_g_score_[current.node] + neighbor.distance;
-
-        // Does this beat the current best path?
-        if (tentative_g_score < best_g_score_[neighbor.node]) {
-          // Yes! Remember this path
-          best_g_score_[neighbor.node] = tentative_g_score;
-          best_parents_[neighbor.node] = current.node;
-
-          // Add the neighbor to the open set
-          auto c = CandidateNode(neighbor.node, tentative_g_score, h_(neighbor.node, destination));
-          // std::cout << "push " << c << std::endl;
-          open_set_.push(c);
-        }
-      }
-    }
-
-    return false;
-  }
-
-
-} // namespace astar
+}  // namespace astar
